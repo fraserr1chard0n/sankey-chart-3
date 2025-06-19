865c84e00f962e3a2490a145eb9f3de60489121d
fraserr1chard0n <fraser.richardson@fi-group.com>
2025-06-19 15:36:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,69 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ResponsiveSankey } from "@nivo/sankey";
+import "./index.css";
 
+const data = {
+  nodes: [
+    { id: "Gov Industrial Strategy" },
+    { id: "Innovate UK" },
+    { id: "ESA" },
+    { id: "MoD" },
+    { id: "NIHR" },
+    { id: "Grant Funding" },
+    { id: "CapEx" },
+    { id: "R&D" },
+    { id: "Energy & Net Zero" },
+    { id: "Undefined CapEx" },
+    { id: "Automotive & Manufacturing" },
+    { id: "Aerospace & Defence" },
+    { id: "Medical" },
+    { id: "Software" },
+    { id: "Others" }
+  ],
+  links: [
+    { source: "Gov Industrial Strategy", target: "Grant Funding", value: 1400 },
+    { source: "Innovate UK", target: "Grant Funding", value: 910 },
+    { source: "ESA", target: "Grant Funding", value: 85 },
+    { source: "MoD", target: "Grant Funding", value: 50 },
+    { source: "NIHR", target: "Grant Funding", value: 50 },
+    { source: "Grant Funding", target: "CapEx", value: 1400 },
+    { source: "Grant Funding", target: "R&D", value: 1095 },
+    { source: "CapEx", target: "Energy & Net Zero", value: 1325 },
+    { source: "CapEx", target: "Undefined CapEx", value: 350 },
+    { source: "R&D", target: "Automotive & Manufacturing", value: 293 },
+    { source: "R&D", target: "Aerospace & Defence", value: 260 },
+    { source: "R&D", target: "Medical", value: 156 },
+    { source: "R&D", target: "Software", value: 74 },
+    { source: "R&D", target: "Others", value: 37 }
+  ]
+};
+
+const Chart = () => (
+  <div style={{ height: "100vh", padding: "2rem", background: "#fff" }}>
+    <ResponsiveSankey
+      data={data}
+      margin={{ top: 40, right: 160, bottom: 40, left: 50 }}
+      align="justify"
+      colors={{ scheme: "category10" }}
+      nodeOpacity={1}
+      nodeThickness={18}
+      nodeSpacing={24}
+      nodeBorderWidth={1}
+      nodeBorderColor={{ from: "color", modifiers: [["darker", 0.8]] }}
+      linkOpacity={0.4}
+      linkHoverOpacity={0.6}
+      linkBlendMode="multiply"
+      labelPosition="outside"
+      labelOrientation="horizontal"
+      labelPadding={12}
+      labelTextColor={{ from: "color", modifiers: [["darker", 1.2]] }}
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+    />
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Chart />);
